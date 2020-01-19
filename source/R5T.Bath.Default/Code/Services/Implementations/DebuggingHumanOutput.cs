64337898ab4866adc922306f0148037b5a24c84b
MinexAutomation <minex.automation@gmail.com>
2020-01-19 15:46:44 -0800

    Added TextWriter-based and debugging-specific IHumanOutput service implementations.

---
+++ source/R5T.Bath.Default/Code/Services/Implementations/DebuggingHumanOutput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace R5T.Bath.Default
+{
+    /// <summary>
+    /// An <see cref="IHumanOutput"/> implementation for use during debugging.
+    /// When an asynchronous output to a human operation is performed, program execution can continue until the operation is awaited. This means that the view of program execution offered to the human can be delayed relative to the actual execution of the program. This can be terribly misleading.
+    /// This debugging-specific version of the <see cref="IHumanOutput"/> service implements "sync-over-async" to make asynchronous operations synchronous.
+    /// This service should be added to a DI-container via a factory method that specifies which <see cref="IHumanOutput"/> service instance should be used internally.
+    /// </summary>
+    public class DebuggingHumanOutput : IHumanOutput
+    {
+        private IHumanOutput HumanOutput { get; }
+
+
+        public DebuggingHumanOutput(IHumanOutput humanOutput)
+        {
+            this.HumanOutput = humanOutput;
+        }
+
+        public void WriteLine(string line)
+        {
+            this.HumanOutput.WriteLine(line);
+        }
+
+        public Task WriteLineAsync(string line)
+        {
+            this.HumanOutput.WriteLineAsync(line).Wait(); // Make the asynchronous operation synchronous.
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ source/R5T.Bath.Default/Code/Services/Implementations/TextWriterHumanOutput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace R5T.Bath.Default
+{
+    /// <summary>
+    /// Note: Creator is responsible for disposal of the constructor-provided <see cref="System.IO.TextWriter"/> instance.
+    /// </summary>
+    public class TextWriterHumanOutput : IHumanOutput
+    {
+        protected TextWriter TextWriter { get; set; }
+
+
+        public TextWriterHumanOutput(TextWriter textWriter)
+        {
+            this.TextWriter = textWriter;
+        }
+
+        protected TextWriterHumanOutput()
+        {
+        }
+
+        public void WriteLine(string line)
+        {
+            this.TextWriter.WriteLine(line);
+        }
+
+        public async Task WriteLineAsync(string line)
+        {
+            await this.TextWriter.WriteLineAsync(line);
+        }
+    }
+}
